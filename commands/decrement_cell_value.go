6291db75f8cbebe86b1d3a6cb43a90a768d739db
Magdalena Andruszak <m.andruszak@matsuu.com>
2020-02-22 11:48:09 +0100

    add decrement cell command

---
+++ commands/decrement_cell_value.go
@@ -0,0 +1,22 @@
+package commands
+
+import (
+	"siiMod/engine"
+)
+
+var _ engine.Command = (*decrementCellValue)(nil)
+
+func NewDecrementCellValue(memoryAccess engine.MemoryAccess) *decrementCellValue {
+	return &decrementCellValue{
+		memoryAccess: memoryAccess,
+	}
+}
+
+type decrementCellValue struct {
+	memoryAccess engine.MemoryAccess
+}
+
+func (s *decrementCellValue) Execute() error {
+	s.memoryAccess.DecrementCellValue()
+	return nil
+}
